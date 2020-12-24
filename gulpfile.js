7b8b54e9b91409fe5c832d7b30dd94c6fa1c0388
mpdragan <mpdragan@ukr.net>
2020-12-24 19:32:55 +0200

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,31 @@ let gulp = require('gulp'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-convat'),
-    uglify = reguire('gulp-uglify'),
-    cssmin = reguire('gulp-cssmin');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin');
+
 
 gulp.task('sass', function(){
-   return gulp.src('app/scss/style.scss')
+   return gulp.src('app/scss/**/*.scss')
       .pipe(sass({outputStyle:'compressed'}))
       .pipe(rename({suffix:'.min'}))
       .pipe(autoprefixer({
         overrideBrowserslist:['last 8 versions']
       }))
       .pipe(gulp.dest('app/css'))
-      .pipe(browserSync.reload({stream:true}))
+      .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('style', function(){
     return gulp.src([
-       'node_modules/normalize.css/nomalize.css',
+       'node_modules/normalize.css/normalize.css',
        'node_modules/slick-carousel/slick/slick.css',
        'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
     .pipe(concat('libs.min.css'))
-    .pipe()
-    .pipe(gulp.dest('app/css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('script', function(){
@@ -36,18 +37,18 @@ gulp.task('script', function(){
      ])
      .pipe(concat('libs.min.js'))
      .pipe(uglify())
-     .pipe(gulp.dest('app/js'))
+     .pipe(gulp.dest('app/js'));
 });
 
 gulp.task('html',function(){
     return gulp.src('app/*.html')
-       .pipe(browserSync.reload({stream:true}))
-})
+       .pipe(browserSync.reload({stream:true}));
+});
 
 gulp.task('js',function(){
     return gulp.src('app/js/*.js')
-       .pipe(browserSync.reload({stream:true}))
-})
+       .pipe(browserSync.reload({stream:true}));
+});
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -58,9 +59,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch',function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/*.js', gulp.parallel('js'));
 });
 
-gulp.task('default',  gulp.parallel('style','script','sass','watch','browser-sync'))
+gulp.task('default',  gulp.parallel('style','script','sass','watch','browser-sync'));
